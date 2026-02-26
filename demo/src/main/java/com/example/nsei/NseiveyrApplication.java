6a9750abaf5f2632d7940c888fb08f849eaee43d
unknown <dudahpereira13@gmail.com>
2026-02-26 19:32:46 -0300

    first commit

---
+++ demo/src/main/java/com/example/nsei/NseiveyrApplication.java
@@ -0,0 +1,13 @@
+package com.example.nsei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NseiveyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NseiveyrApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/nsei/NseiveyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nsei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NseiveyrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
